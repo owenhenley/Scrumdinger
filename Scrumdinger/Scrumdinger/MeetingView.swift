27443374d8fc8f5458c718bc7e495da3245c9f54
Owen Henley <owenhenley@me.com>
2023-02-27 10:48:04 +0000

    Add initial views

---
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Owen Henley on 27/02/2023.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                VStack {
+                    
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "ourglass.tophalf.fill")
+                }
+            }
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Owen Henley on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
