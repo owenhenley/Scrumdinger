53916751ea36060ac13b9191eb0c2b4a41337f8a
Owen Henley <owenhenley@me.com>
2023-02-28 17:43:41 +0000

    listing scrums

---
+++ Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -0,0 +1,28 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Owen Henley on 28/02/2023.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums, id: \.title) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
