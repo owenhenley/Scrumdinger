737a3f5d24bf46c206b75b81c3608e7d9f10645f
Owen Henley <owenhenley@me.com>
2023-02-28 17:33:24 +0000

    Make the card view accessible

--- Scrumdinger/Scrumdinger/Theme.swift
+++ Scrumdinger/Scrumdinger/Theme.swift
@@ -34,6 +34,15 @@ enum Theme: String {
         }
     }
     
+    var accessibleFontColor: Color {
+        switch self {
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow:
+            return .black
+        case .indigo, .magenta, .navy, .oxblood, .purple:
+            return .white
+        }
+    }
+    
     var mainColor: Color {
         Color(rawValue)
     }
