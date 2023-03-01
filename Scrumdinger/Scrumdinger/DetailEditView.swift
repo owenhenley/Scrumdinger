97e803b28b2142b0ed823c905ada8cab4a78cf61
Owen Henley <owenhenley@me.com>
2023-03-01 15:49:04 +0000

    add a detail edit view

---
+++ Scrumdinger/Scrumdinger/DetailEditView.swift
@@ -0,0 +1,34 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Owen Henley on 01/03/2023.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    
+    @State private var data = DailyScrum.Data()
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $data.title)
+                HStack {
+                    Slider(value: $data.lengthInMinutes, in: 1...30, step: 1) {
+                        Text("Length")
+                    }
+                    Spacer()
+                    Text("\(Int(data.lengthInMinutes)) minutes")
+                }
+            }
+        }
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
