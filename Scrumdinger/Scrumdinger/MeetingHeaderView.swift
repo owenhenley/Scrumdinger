e84bb3d25c4ab77234df257cf0ba9b00cfe30d34
Owen Henley <owenhenley@me.com>
2023-03-02 10:10:48 +0000

    add meeting header view, move some logic out of meeting view

---
+++ Scrumdinger/Scrumdinger/MeetingHeaderView.swift
@@ -0,0 +1,54 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by Owen Henley on 02/03/2023.
+//
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    let secondsElapsed: Int
+    let secondsRemaining: Int
+    
+    private var totalSeconds: Int {
+        secondsElapsed + secondsRemaining
+    }
+    
+    private var progress: Double {
+        guard totalSeconds > 0 else { return 1 }
+        return Double(secondsElapsed) / Double(secondsRemaining)
+    }
+    
+    private var minutesRemaining: Int {
+        secondsRemaining / 60
+    }
+    
+    var body: some View {
+        VStack {
+            ProgressView(value: progress)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("\(secondsElapsed)", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("\(secondsRemaining)", systemImage: "ourglass.tophalf.fill")
+                }
+            }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("Time Remaining: \(minutesRemaining) minutes")
+    }
+}
+
+struct MeetingHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingHeaderView(secondsElapsed: 15, secondsRemaining: 30)
+            .previewLayout(.sizeThatFits)
+    }
+}
--- Scrumdinger/Scrumdinger/MeetingView.swift
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -15,23 +15,6 @@ struct MeetingView: View {
             RoundedRectangle(cornerRadius: 16.0)
                 .fill(scrum.theme.mainColor)
             VStack {
-                ProgressView(value: 5, total: 15)
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("Seconds Elapsed")
-                            .font(.caption)
-                        Label("300", systemImage: "hourglass.bottomhalf.fill")
-                    }
-                    Spacer()
-                    VStack(alignment: .trailing) {
-                        Text("Seconds Remaining")
-                            .font(.caption)
-                        Label("600", systemImage: "ourglass.tophalf.fill")
-                    }
-                }
-                .accessibilityElement(children: .ignore)
-                .accessibilityLabel("Time Remaining")
-                .accessibilityLabel("10 Minutes")
                 Circle()
                     .strokeBorder(lineWidth: 24)
                 HStack {
