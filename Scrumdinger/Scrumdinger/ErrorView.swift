4c8f327bbe891a8099f493dd80aa1bec22955603
Owen Henley <owenhenley@me.com>
2023-03-06 17:46:59 +0000

    setup error screen

--- Scrumdinger/Scrumdinger/ErrorView.swift
+++ Scrumdinger/Scrumdinger/ErrorView.swift
@@ -9,22 +9,33 @@ import SwiftUI
 
 struct ErrorView: View {
     let errorWrapper: ErrorWrapper
+    @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        VStack {
-            Text("This is an Error Message")
-                .font(.title)
-                .padding(.bottom)
-            Text("Error Description is that this is an error")
-                .font(.headline)
-            Text("This is guidence, to remove the error something has to work")
-                .font(.caption)
-                .padding(.top)
-            Spacer()
-        }
-        .padding()
-        .background(.ultraThinMaterial)
+        NavigationView {
+            VStack {
+                Text("This is an Error Message")
+                    .font(.title)
+                    .padding(.bottom)
+                Text("Error Description is that this is an error")
+                    .font(.headline)
+                Text("This is guidence, to remove the error something has to work")
+                    .font(.caption)
+                    .padding(.top)
+                Spacer()
+            }
+            .padding()
+            .background(.ultraThinMaterial)
         .cornerRadius(16)
+        }
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button("Dismiss") {
+                    dismiss()
+                }
+            }
+        }
     }
 }
 
